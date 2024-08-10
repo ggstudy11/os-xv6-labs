ce1395586f6ee0726819c3916de6caea143db175
ggstudy1 <439403849@qq.com>
2024-08-10 20:41:08 +0800

    lab1-2

--- user/pingpong.c
+++ user/pingpong.c
@@ -12,27 +12,30 @@ int main(int argc, char** argv)
 {
     // fork to create a child process
     
-    int d1[2]; // one direction communicate
-    int d2[2]; // another direction communicate
+    int fd1[2]; // one direction communicate
+    int fd2[2]; // another direction communicate
     
     // a couple of pipes for communication
-    pipe(d1);
-    pipe(d2);
+    pipe(fd1);
+    pipe(fd2);
 
     // buffer
     char buf[512];
 
-    int pid = fork();
+    int pid = fork(); // pid
      
     if(pid == 0){
-        read(d1[0], buf, 4);
-        printf("%d: received %s\n", getpid(), buf);
-        write(d2[1], "pong", 4);
+        // child
+        read(fd1[0], buf, 4); // read
+        printf("%d: received %s\n", getpid(), buf); // print msg
+        write(fd2[1], "pong", 4); // write
+
     } else {
-        write(d1[1], "ping", 4);
-        read(d2[0], buf, 4);
-        wait(0);
-        printf("%d: received %s\n", getpid(), buf);
+        // parent
+        write(fd1[1], "ping", 4); // write
+        read(fd2[0], buf, 4); // read
+        wait(0); // no confict in output
+        printf("%d: received %s\n", getpid(), buf); // print msg
     }
     exit(0);
 }
