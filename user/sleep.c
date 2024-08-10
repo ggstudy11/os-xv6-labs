a15f4226733074087b0e32eb62165f5fe2f30a8a
ggstudy1 <439403849@qq.com>
2024-08-10 20:22:50 +0800

    lab1-1

--- user/sleep.c
+++ user/sleep.c
@@ -1,15 +1,24 @@
+// types header
 #include "kernel/types.h"
+// sys func header
 #include "user/user.h"
 
+// argc for num, argv for string
 int main(int argc, char** argv)
 {
     // sleep ticks
     int ticks;
+
     if (argc <= 1) {
-        printf("usage: sleep n(ticks)\n");
-        exit(0);
+        fprintf(2, "usage: sleep n(ticks)\n");
+        // error exit
+        exit(1);
     }
+
+    // string to num
     ticks = atoi(argv[1]);
+    // sys sleep
     sleep(ticks);
+    // normal exit
     exit(0);
 }
