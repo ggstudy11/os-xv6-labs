382a9a2c949c70148247fa0403503419feb5583d
ggstudy11 <439403849@qq.com>
2024-08-21 18:12:25 +0800

    lab1-T4

--- user/find.c
+++ user/find.c
@@ -3,24 +3,6 @@
 #include "kernel/stat.h"
 #include "kernel/fs.h"
 
-char*
-fmtname(char *path)
-{
-  static char buf[DIRSIZ+1];
-  char *p;
-
-  // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
-    ;
-  p++;
-
-  // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
-    return p;
-  memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-  return buf;
-}
 
 void find(char* dir, char* file) {
 
@@ -47,54 +29,45 @@ void find(char* dir, char* file) {
         return;
     }
 
-    if (strlen(dir) + 1 + DIRSIZ + 1 > sizeof buf) {
-
+    if (strlen(dir) + 1 + DIRSIZ + 1 > sizeof (buf)) {
         fprintf(2, "find: directory too long\n");
-        // 关闭文件描述符 fd
         close(fd);
         return;
     }
 
-     strcpy(buf, dir);
-    // buf 是一个绝对路径，p 是一个文件名，通过加 "/" 前缀拼接在 buf 的后面
+    strcpy(buf, dir);
     p = buf + strlen(buf);
     *p++ = '/';
-    // 读取 fd ，如果 read 返回字节数与 de 长度相等则循环
     while (read(fd, &de, sizeof(de)) == sizeof(de))
     {
         if(de.inum == 0)
             continue;
-        // 不要递归 "." 和 "..."
         if (!strcmp(de.name, ".") || !strcmp(de.name, ".."))
             continue;
-        // memmove，把 de.name 信息复制 p，其中 de.name 代表文件名
         memmove(p, de.name, DIRSIZ);
-        // 设置文件名结束符
         p[DIRSIZ] = 0;
         if(stat(buf, &st) < 0)
         {
-            // 出错则报错
             fprintf(2, "find: cannot stat %s\n", buf);
             continue;
         }
-        // 如果是目录类型，递归查找
         if (st.type == T_DIR)
         {
             find(buf, file);
-        }
-        // 如果是文件类型 并且 名称与要查找的文件名相同
-        else if (st.type == T_FILE && !strcmp(de.name, file))
+        }else if (st.type == T_FILE && !strcmp(de.name, file))
         {
-            // 打印缓冲区存放的路径
             printf("%s\n", buf);
         } 
     }
+
+    close(fd);
 }
 
 int main(int argc, char** argv)
 {
+    // find dirName fileName
     if (argc != 3) {
-        printf("usage: find dirName fileName\n");
+        printf("usage: find dirname filename\n");
         exit(1);
     } 
     find(argv[1], argv[2]);
